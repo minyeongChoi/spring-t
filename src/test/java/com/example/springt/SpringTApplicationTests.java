d35263b6b44bc04ec7ead7403cda47fcb4c3d5ae
minyeongChoi <42730692+minyeongChoi@users.noreply.github.com>
2021-06-21 03:20:53 +0900

    Initial commit

---
+++ src/test/java/com/example/springt/SpringTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
